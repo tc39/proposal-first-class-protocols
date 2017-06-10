64ea74b4dd3cbd83e84f13bbc0611170416ea2b5
Michael Ficarra <git@michael.ficarra.me>
2017-06-10 10:35:51 -0700

    add set-using-hashCode example

---
+++ examples/set-using-hashCode.js
@@ -0,0 +1,26 @@
+interface HasHashCode {
+  hashCode;
+}
+
+class SetUsingHashCode extends Set {
+  constructor(iterable) {
+    super();
+    this.#map = new Map;
+    for (let x of iterable) {
+      this.add(x);
+    }
+  }
+
+  add(value) {
+    this.#map.set(value[HasHashCode.hashCode](), value);
+  }
+
+  // ... others
+}
+
+class Identity implements HasHashCode {
+  [HashHashCode.hashCode]() { return this.val; }
+  constructor(val) { this.val = val; }
+}
+
+new SetUsingHashCode().add(new Identity(1)).has(new Identity(1)) // true
